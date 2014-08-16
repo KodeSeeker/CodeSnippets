c8cc2a024ad7ba287ffad3df9861d4b4a67f8316
Vivek Mahadevan <vivek_veeramani@intuit.com>
2014-08-15 22:56:34 -0700

    fix 2

--- BFSMaze.java
+++ BFSMaze.java
@@ -6,28 +6,33 @@ public Queue<Coordinates> q = new LinkedList<Cooridnates>();
 
 public static  int[] rowIndices= new int[]{-1,-1,-1,0,0,1,1,1};
 public static  int[] colIndices= new int[]{-1,0,1,-1,1,-1,0,1};
+public static boolean[][] visited;
+
 //x and y are the start coordinates
 public boolean  BFS (int [] [] in, int x,int y, Coordinate target)
 {
 	Coordinate c= new Coordinate(x,y); //let c be the origin.
-	q.push(c);
+	visited= new boolean [in.length][in[0].length];
+	
+	Queue <Coordinate> q = new LinkedList<Coordinate>();
+	
+	q.add(c);
+	
 	while(!q.isEmpty())
 	{
-		Coordinate  n = q.pop();
-		n.visited =true;
-		if (n==target)
-			return true;
+		Coordinate n= q.pop();
+		visited[n.x][n.y]=true;
+		if(n ==target)
+		  return true;
 		else
 		{
-		for (int k=0;k<8;k++)
+		   for (int k=0;k<8;k++)
 			{
-			//	q.add( new Coordinate(x+rowIndex[k], y+colIndex[k]);
-				Coordinate temp= new Coordinate(x+rowIndex[k], y+colIndex[k] );
-				if(!temp.visited )
-					{
-						q.add(temp);
-					}
+				if(!visited[n.x+rowIndices[k]][n.y+colIndices[k]])
+					q.add(new Coordinate( [n.x+rowIndices[k]],n.y+colIndices[k]]);
 			}
 		}
 	}
+
+	return false;
 }
