7a5e3a94a9faf6863fc894fa15995cbb0692bee8
Vivek Mahadevan <vivek_veeramani@intuit.com>
2014-08-15 22:31:57 -0700

    fix

--- BFSMaze.java
+++ BFSMaze.java
@@ -7,7 +7,7 @@ public Queue<Coordinates> q = new LinkedList<Cooridnates>();
 public static  int[] rowIndices= new int[]{-1,-1,-1,0,0,1,1,1};
 public static  int[] colIndices= new int[]{-1,0,1,-1,1,-1,0,1};
 //x and y are the start coordinates
-public boolean  BFS (int [] [] in, int x,int y)
+public boolean  BFS (int [] [] in, int x,int y, Coordinate target)
 {
 	Coordinate c= new Coordinate(x,y); //let c be the origin.
 	q.push(c);
