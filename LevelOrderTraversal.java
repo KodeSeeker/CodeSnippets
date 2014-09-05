9449b1d1b8a338e5557c9ffd483a1a61210af517
Vivek Veeramani <Vivek_Veeramani@intuit.com>
2014-09-04 22:29:31 -0700

    fixes

--- LevelOrderTraversal.java
+++ LevelOrderTraversal.java
@@ -14,34 +14,37 @@ Solution : Breadth first traversal.
 
 public void printSocialGraph(Member m)
 {
-  if (m== null)
-    return;
-    //BFS -Queue
-  Queue <Member> q= new LinkedList<Member>();
-  // Use a HashMap to store the level of the member friend.
-  Map<Member,Integer> map= new HashMap<Member,Integer>();
-  level=-1;
-  map.put(m,0); // m is at level 0.
-  q.add(m);
+  	if (m== null)
+    		return; // base case.
+    	//BFS -Queue
+  	Queue <Member> q= new LinkedList<Member>();
   
+	// Use a HashMap to store the level of the member friend.
+ 	 Map<Member,Integer> map= new HashMap<Member,Integer>();
+  	 level=-1;
+  	 map.put(m,0); // m is at level 0.
+  	 q.add(m);
+  
+
   //BFS
   while(!q.isEmpty()
   {
-    Member current= q.poll();
-    //check the level- this prevents reprinting of a member.
-    if(map.get(current)>level)
-    {
-      Sysout("Level"+ ++level);// print the current level.
-    }
-  
-  // now print the member
-  Sysout ("Member" +curr.name);
-  
-  // Add the members too
-  for(  Member friend: curr.friends)
-  {
-    map.put(friend,++level);
-    q.add(friend);
-  }
-  
+    	Member current= q.poll();
+    	//check the level- this prevents reprinting of a member.
+   	 if(map.get(current)>level)
+ 		{
+			//print the level .
+			System.out.println("Level" + level);	
+ 		}
+	//print the member
+	System.out.println("Member name " +current.name);
+	
+	//add his friends to the queue.
+	for (Member frnd: current.friends)
+	{
+		//add to the queue and set the level in the map.
+		q.add(frnd);
+		map.put(frnd,++level);// increment the level. and add to map.
+	}
+
   }  
