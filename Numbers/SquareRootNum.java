0ee63972275d5d9fdca63f910fa62b1a43cebb05
Vivek Mahadevan <KodeSeeker@users.noreply.github.com>
2018-02-14 09:26:01 -0800

    Update SquareRootNum.java
    Update to correct version of code that handles edge cases.

--- Numbers/SquareRootNum.java
+++ Numbers/SquareRootNum.java
@@ -1,32 +1,44 @@
 // find the square root of a number with sufficient precision.
 //O(log N) time. Binary search
 
-public double sqrt(double a){
-    
-    // edge cases
-    if(a<0)
-        return -1;//invalid input
-    if(a==0||a==1)
-        return a;//edge case 2
-    // core
-    int start = 0;
-    int end = a;
-    // based on fact that if (a>1) then 0<sqrt(a)<a
-    //if(a<1) then 0<a<sqrt(a)
-    int precision = 0.000001;
-    //check for value of a
-    if(a<1)
-        end=1;
-    while(end-start<precision){
-        int mid= (start+end) >>>1;
-        int midSqr=mid*mid;
-        if(midSqr==a) 
-            return mid;// we found the square root and the number was a perfect square
-        if(midSqr<a)
-            start=mid;
-        else if (midSqr>a)
-            end =mid;
-    }
-    // if all else fails
-    return (start+end) >>>1;
+ int mySqrt(int x) {
+	        
+	        //Key insight - 
+	        // if a > 1 ; a>sqrt(A).
+	        // If 0<a<1.. 0<a<sqrt(A) <1. eg. a= 0.25
+	        // since we just want to return the integer part we can simply return 0. 
+	        if(x<=0)
+	            return x;
+	        if(x<1)
+	            return 0;
+	        int end = x;
+	        int start = 0;
+	        int ans =0;
+	        while (start <= end) {
+	            int mid = (start + end) >>>1;// end = 9 . mid = 4. ;end =3
+	       // Use bigInt to avoid overflow of midSqr if x and mid end up being too large for int or long!
+            BigInteger midi = BigInteger.valueOf(mid);
+	        BigInteger midSqr = midi.multiply(midi);
+                
+	        BigInteger xAsBigInt = BigInteger.valueOf(x);
+
+	            if( midSqr.intValue() == x ) { // perfect square
+	                return mid;
+	            }
+	            if(midSqr.compareTo(xAsBigInt) >0) { //overshot value of x.
+	                end = mid -1;
+	            }
+	            else { //midSqr <x - underestimated value of x
+	                start = mid+1;
+	                ans = mid;
+                 // Store value of mid as the output as best effort value because mid could likely be 
+                 //   overwritten to a wrong value by a change in start and end.
+                 //Also, we update it in this branch instead of the other branch because we want to round down the value of the root 
+                 // if its not a perfect square.
+	                
+	            }
+	        }
+	        
+	        return ans;
+	    }
 }
